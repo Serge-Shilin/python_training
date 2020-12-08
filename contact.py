39e7ff47b0402a43c49b081d9977c1a30b26bf6a
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2020-12-08 12:13:30 +0300

    вспомогательный класс

---
+++ contact.py
@@ -0,0 +1,17 @@
+
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, address, homephone, mobile, workphone, mail, bday, bmounth, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.address = address
+        self.homephone = homephone
+        self.mobile = mobile
+        self.workphone = workphone
+        self.mail = mail
+        self.bday = bday
+        self.bmounth = bmounth
+        self.byear = byear
