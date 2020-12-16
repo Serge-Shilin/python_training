26d6788e2112543faa136c602160934f74544c6d
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2020-12-16 11:29:00 +0300

    убрал дублирующие методы

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -1,7 +1,7 @@
-
+from model.group import Group
 
 
 def test_update_first_group(app):
     app.session.login("secret", "admin")
-    app.group.update_first_group()
+    app.group.create(Group(u"11111", u"11111", u"11111"))
     app.session.logout()
--- test_contact/test_update_contact.py
+++ test_contact/test_update_contact.py
@@ -3,7 +3,8 @@ from model.contact import Contact
 
 def test_contact_update(app):
     app.session.login(username="admin", password="secret")
-    app.contact.test_contact_update(Contact(firstname="1111", middlename="Сергеевич", lastname="Сергеев", nickname="Серега", address="г. Казань",
+    app.contact.fill_contact_form(
+        Contact(firstname="11111", middlename="2222222", lastname="3333333", nickname="444444", address="55555",
                 homephone="11111", mobile="22222",
-                workphone="333333", mail="sergei@gmail.com", bday="2", bmounth="May", byear="1975"))
+                workphone="333333", mail="22222", bday="1", bmounth="May", byear="1901"))
     app.session.logout()
