5c2df3afa8a979bc0feed0b41c7c5575d03a5b2a
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2020-12-28 16:58:43 +0300

    проверка равенства объектов и списков

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,10 +1,12 @@
 
 from model.group import Group
 
-def test_delete_first_group(app):
+def test_delete_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    old_groups = app.group.get_contacts_list()
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
-    new_groups = app.group.get_contacts_list()
+    new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []
+    assert old_groups == new_groups
