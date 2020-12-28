eff6739e3798eb56e300bfaf56617de67a18c015
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2020-12-28 17:30:41 +0300

    сортировка списков перед сравнением = создание групп, контактов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_add_group(app):
-    old_groups = app.group.get_contacts_list()
-    app.group.create(Group(u"группа", u"группа", u"группа"))
-    new_groups = app.group.get_contacts_list()
+    old_groups = app.group.get_group_list()
+    group = Group(u"группа", u"группа", u"группа")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
