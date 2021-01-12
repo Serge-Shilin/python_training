c7298154dd428a8501e85dcc6826cddeb752f1f5
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2021-01-12 18:38:44 +0300

    частично выполнил 18,19 задания часть вторая

--- test_contact/new_contact_test.py
+++ test_contact/new_contact_test.py
@@ -1,25 +1,8 @@
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", middlename="", lastname="", nickname="", address="",
-                homephone="", mobile="",
-                work="", fax="", mail="", bday="", bmounth="", byear="")] + [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20), lastname=random_string("lastname", 20), address=random_string("address", 30),
-            homephone=random_string("homephone", 11), mobile=random_string("mobile", 12), work=random_string("work", 10), fax=random_string("fax", 10),
-            mail=random_string("mail", 15))
-    for i in range(5)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_new_contact(app, contact):
+def test_new_contact(app, json_contact):
+    contact = json_contacts
     old_contacts = app.contact.get_contacts_list()
     app.contact.contact_create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
