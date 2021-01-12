a8688e5247dbb2f73d08c3d8f479664eda47586d
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2021-01-12 19:50:48 +0300

    немного привел в порядок

--- test_contact/test_update_contact.py
+++ test_contact/test_update_contact.py
@@ -21,16 +21,3 @@ def test_contact_firstname(app):
     old_contacts[index] = contact
     #assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-#def test_contact_update_lastname(app):
-    #old_contacts = app.contact.get_contacts_list()
-    #if app.contact.count() == 0:
-        #app.contact.contact_create(
-            #Contact(firstname="Сергей", middlename="Сергеевич", lastname="Сергеев", nickname="Серега",
-                    #address="г. Казань",
-                    #homephone="11111", mobile="22222",
-                    #workphone="333333", mail="sergei@gmail.com", bday="", bmounth="May", byear="1975"))
-    #app.contact.contact_update(
-        #Contact(firstname="sssssss", middlename="ssss", lastname="sssssss", nickname="sssssss", address="ssssss", homephone="99999", mobile="999999",
-                #workphone="99999", mail="@", byear="1901"))
-    #new_contacts = app.contact.get_contacts_list()
-    #assert len(old_contacts) == len(new_contacts)
