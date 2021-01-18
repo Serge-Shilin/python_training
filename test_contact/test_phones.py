cecb6c3ad2ae6f5c9b779496d87dcfd9361eaaec
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2021-01-19 01:05:17 +0300

    ОРМ и задание 21

--- test_contact/test_phones.py
+++ test_contact/test_phones.py
@@ -1,4 +1,6 @@
 import re
+from model.contact import Contact
+from random import randrange
 
 
 def test_phones_on_home_page(app):
@@ -16,9 +18,17 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.fax == contact_from_edit_page.fax
 
 
-def test_other_on_home_page(app):
-    contact_from_home_page = app.contact.get_contacts_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+def test_other_on_home_page(app, db):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов",
+                                   homephone="111", mobile="111", work="111",
+                                   address="Казань",
+                                   fax="111", email="111u", email2="111",
+                                   email3="111"))
+    list_contact = app.contact.get_contacts_list()
+    index = randrange(len(list_contact))
+    contact_from_home_page = app.contact.get_contacts_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_emails_from_home_page == merge_all_emails_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.lastname == merge_lastname_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.firstname == merge_firstname_like_on_home_page(contact_from_edit_page)
