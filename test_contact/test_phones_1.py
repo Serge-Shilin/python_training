72953ce88b85e0204a9f04ebccc23052d2fa9ae1
Nikolay.Kukarkin <n.kukarkin1991@gmail.com>
2021-01-23 09:42:02 +0300

    исправленное 21.2

--- test_contact/test_phones_1.py
+++ test_contact/test_phones_1.py
@@ -21,47 +21,7 @@ def test_contact_info_home_vs_db(app, db):
     assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
 
 
-def test_contact_info_edit_vs_db(app, db):
-    if app.contact.count() == 0:
-        app.contact.contact_create(
-            Contact(firstname="Сергей", middlename="Сергеевич", lastname="Сергеев", nickname="Серега",
-                    address="г. Казань",
-                    homephone="11111", mobile="22222", fax="121212",
-                    work="333333", email="sergei@gmail.com", email2=1212, email3=1221, bday="", bmounth="May", byear="1975"))
-    if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов",
-                                   homephone="111", mobile="111", work="111",
-                                   address="Казань",
-                                   fax="111", email="111u", email2="111",
-                                   email3="111"))
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page()
-    contact_from_db = db.get_contact_list()
-    assert sorted(contact_from_edit_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
-
-
-
-#def test_contact_info_home_vs_edit(app, db):
-    #if len(db.get_contact_list()) == 0:
-        #app.contact.create(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов",
-                                   #homephone="111", mobile="111", work="111",
-                                   #address="Казань",
-                                   #fax="111", email="111u", email2="111",
-                                   #email3="111"))
-    #list_contacts = app.contact.get_contacts_list()
-    #index = randrange(len(list_contacts))
-    #contact_from_home_page = db.get_contact_list()[index]
-    #db.get_contact_list() = app.contact.get_contact_info_from_edit_page()
-    #contact_from_home_page = app.contact.get_contacts_list()[index]
-    #contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    #assert sorted(contact_from_home_page, key=Contact.id_or_max) == sorted(contact_from_db, key=Contact.id_or_max)
-    #assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    #assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    #assert contact_from_home_page.address == contact_from_edit_page.address
-    #assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_from_home_page(contact_from_edit_page)
-    #assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_from_home_page(contact_from_edit_page)
-
-
-def test_contact_info_home_vs_view(app, db):
+def test_contact_info_home_vs_viev(app, db):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Иван", middlename="Иванович", lastname="Иванов",
                                    homephone="111", mobile="111", work="111",
